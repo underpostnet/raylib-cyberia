44104398020e36e88ac176fa5646bf73435da67c
fcoverdugo <fcoverdugoa@underpost.net>
2023-02-17 14:14:30 -0300

    update

--- cyberia.c
+++ cyberia.c
@@ -1,24 +1,32 @@
 #include "raylib.h"
-#include <stdio.h> // Required for: fopen(), fclose(), fputc(), fwrite(), printf(), fprintf(), funopen()
-#include <time.h>  // Required for: time_t, tm, time(), localtime(), strftime()
+#include <stdio.h>  // Required for: fopen(), fclose(), fputc(), fwrite(), printf(), fprintf(), funopen()
+#include <time.h>   // Required for: time_t, tm, time(), localtime(), strftime()
+#include <string.h> // Required for: strcmp()
 
 #define SCREEN_RANGE_MAP 30
 #define SCREEN_FACTOR_RENDER 15
 #define SCREEN_DIM_RENDER SCREEN_RANGE_MAP *SCREEN_FACTOR_RENDER
 #define MAX_BOTS 5
+#define MAX_TYPES 1
+#define TYPE_ELEMENT_MAX_CHAR 100
 
 //------------------------------------------------------------------------------------
 // Structs
 //------------------------------------------------------------------------------------
 
-struct bot
+struct element
 {
     int x;
     int y;
     int dim;
+    char *type;
     Color color;
 };
-struct bot bots[MAX_BOTS];
+
+struct element bots[MAX_BOTS];
+
+const char *types[MAX_TYPES] = {"bot"};
+int maxTypes[MAX_TYPES] = {MAX_BOTS};
 
 //------------------------------------------------------------------------------------
 // Program main entry point
@@ -29,20 +37,40 @@ int main(void)
     // https://www.w3schools.com/c/c_data_types.php
     // https://www.raylib.com/examples.html
     // printf("%i \n", GetRandomValue(0, 10));
+    // https://github.com/oz123/awesome-c
+    // https://github.com/felselva/uastar
     //--------------------------------------------------------------------------------------
 
-    for (int i = 0; i < MAX_BOTS; i++)
+    for (int i = 0; i < MAX_TYPES; i++)
     {
-        bots[i].x = GetRandomValue(0, SCREEN_RANGE_MAP);
-        bots[i].y = GetRandomValue(0, SCREEN_RANGE_MAP);
-        bots[i].dim = 2;
-        bots[i].color = YELLOW;
+        for (int j = 0; j < maxTypes[i]; j++)
+        {
+            if (strcmp(types[i], "bot") == 0)
+            {
+                bots[j].type = "bot";
+                bots[j].x = GetRandomValue(0, SCREEN_RANGE_MAP);
+                bots[j].y = GetRandomValue(0, SCREEN_RANGE_MAP);
+                bots[j].dim = 2;
+                bots[j].color = YELLOW;
+            }
+        }
+    }
+
+    for (int i = 0; i < MAX_TYPES; i++)
+    {
+        for (int j = 0; j < maxTypes[i]; j++)
+        {
+            if (strcmp(bots[j].type, "bot") == 0)
+            {
+                printf("succes load -> %s %i \n", bots[j].type, j);
+            }
+        }
     }
 
     InitWindow(SCREEN_DIM_RENDER, SCREEN_DIM_RENDER, "CYBERIA");
     SetTargetFPS(60); // Set our game to run at 60 frames-per-second
-    //--------------------------------------------------------------------------------------
 
+    //--------------------------------------------------------------------------------------
     // Main game loop
     while (!WindowShouldClose()) // Detect window close button or ESC key
     {
